a5559d9eaa82cb2c3d5a395d5d1c862526dc8664
Julian <rodriguezjuliann@outlook.com>
2023-08-27 00:50:50 -0300

    1er entrega sin push final

---
+++ rodriguezjulian-ecomerce/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import Titulo from "../Titulo/Titulo"
+
+const Footer = () =>    <footer>
+                            <Titulo titulo='RODRIGUEZ Julian' subtitulo='React CoderHouse'  />
+                            <h3>
+                                (falta push de bootstrap)
+                            </h3>
+                        </footer>
+
+export default Footer
---
+++ rodriguezjulian-ecomerce/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return <nav>
+                <ul>
+                <li>
+                    <a href="">Planes Musculacion</a>
+                </li>
+                <li>
+                <a href="">Planes Natacion</a>
+                </li>
+                <li>
+                <a href="">Planes Fitness</a>
+                </li>
+                </ul>
+            </nav>
+}
+
+export default Navbar
