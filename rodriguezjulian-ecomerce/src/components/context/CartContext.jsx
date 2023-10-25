4c92ea533d39d9a07a164177dedc7a4b4960d957
Julian <rodriguezjuliann@outlook.com>
2023-10-25 00:36:37 -0300

    actualizar stock

--- rodriguezjulian-ecomerce/src/components/context/CartContext.jsx
+++ rodriguezjulian-ecomerce/src/components/context/CartContext.jsx
@@ -1,98 +1,140 @@
 import { createContext, useContext, useState } from "react";
-import Swal from 'sweetalert2'
-import React from 'react';
-import { toast } from 'react-toastify';
+import Swal from "sweetalert2";
+import React from "react";
+import { toast } from "react-toastify";
+import { collection, getFirestore, addDoc, updateDoc, doc, writeBatch, getDoc } from "firebase/firestore"
 
+const CartContext = createContext([]);
 
+export const useCartContext = () => useContext(CartContext);
+export const CartContextProvider = ({ children }) => {
+  const [cartList, setCartList] = useState([]);
 
+  const isProduct = (id) => cartList.findIndex((prod) => prod.id === id);
 
-const CartContext  = createContext([])
-
-export const useCartContext = () => useContext(CartContext)
-export const CartContextProvider = ({children}) => {
-    const [cartList, setCartList] = useState([])
-
-    const isProduct = (id) => cartList.findIndex(prod => prod.id === id)
-    
-    
-    const addProduct = (newProduct)=>{
-        const index = isProduct(newProduct.id)
-        if (index !== -1) {
-            cartList[index].quantity += newProduct.quantity 
-            setCartList([...cartList]) 
-        } else {
-            setCartList([
-                ...cartList,
-                newProduct
-            ])            
-        }
-
+  const addProduct = (newProduct) => {
+    const index = isProduct(newProduct.id);
+    if (index !== -1) {
+      cartList[index].quantity += newProduct.quantity;
+      setCartList([...cartList]);
+    } else {
+      setCartList([...cartList, newProduct]);
     }
+  };
 
-        // Eliminar por producto
-        const eliminarProducto = (pid) => setCartList(cartList.filter(prod => prod.id !== pid))
+  // Eliminar por producto
+  const eliminarProducto = (pid) =>
+    setCartList(cartList.filter((prod) => prod.id !== pid));
 
-        // mostrar la cantidad de productos total que tienen 
-        const cantidadTotal = ()=> cartList.reduce((cantidadTotal, objProduct)=> cantidadTotal += objProduct.quantity ,0)
+  // mostrar la cantidad de productos total que tienen
+  const cantidadTotal = () =>
+    cartList.reduce(
+      (cantidadTotal, objProduct) => (cantidadTotal += objProduct.quantity),
+      0
+    );
 
-        // precio total (()=>{}, inicializador de precio total)
-        const precioTotal = () => cartList.reduce((precioTotal, objProduct)=> precioTotal += (objProduct.price * objProduct.quantity) ,0)
-    
+  // precio total (()=>{}, inicializador de precio total)
+  const precioTotal = () =>
+    cartList.reduce(
+      (precioTotal, objProduct) =>
+        (precioTotal += objProduct.price * objProduct.quantity),
+      0
+    );
 
+  const deleteCart = () => {
+    toast.success("Se ha vaciado el carrito", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+    setCartList([]);
+  };
 
-    const deleteCart = () => {
-        toast.success('Se ha vaciado el carrito', {
-            position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-            });
-        setCartList([])
-    }
-
-    const confirmarCompra = () =>{
-        Swal.fire({
-          title: '¿Desea confirmar la compra?',
-          background: "rgba(255, 255, 255, 0.80)",
-          color: "black",
-          showDenyButton: true,
-          confirmButtonText: 'Confirmar',
-          denyButtonText: `Cancelar`,
-          confirmButtonColor: "green"
-        }).then((result) => {
-          if (result.isConfirmed) {
+  const confirmarCompra = () => {
+    Swal.fire({
+      title: "¿Desea confirmar la compra?",
+      background: "rgba(255, 255, 255, 0.80)",
+      color: "black",
+      showDenyButton: true,
+      confirmButtonText: "Confirmar",
+      denyButtonText: "Cancelar",
+      confirmButtonColor: "green",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const order = {};
+        order.buyer = {
+          name: "Julian",
+          phone: "123456789",
+          email: "julianrdz@gmail.com",
+        };
+        order.items = cartList.map((prod) => {
+          return {
+            id: prod.id,
+            name: prod.name,
+            price: prod.price,
+            quantity: prod.quantity,
+          };
+        });
+        order.total = precioTotal();
+        const queryDB = getFirestore();
+        const batch = writeBatch(queryDB);
+        order.items.forEach((item) => {
+          const productId = item.id;
+          const quantityPurchased = item.quantity;
+          const productRef = doc(queryDB, "products", productId);
+          getDoc(productRef)
+            .then((productDoc) => {
+              if (productDoc.exists()) {
+                const productData = productDoc.data();
+                const currentStock = productData.stock;
+                if (currentStock >= quantityPurchased) {
+                  const newStock = currentStock - quantityPurchased;
+                  const productBatch = writeBatch(queryDB);
+                  productBatch.update(productRef, {
+                    stock: newStock,
+                  });
+                  productBatch
+                    .commit()
+                }
+              }
+            })
+        });
+        batch
+          .commit()
+          .then(() => {
             Swal.fire({
-              position: 'center',
-              title: 'Muchas gracias por tu compra ',
+              position: "center",
+              title: "Muchas gracias por tu compra",
               text: "✔️✔️✔️",
               background: "rgba(255, 255, 255, 0.80)",
               color: "black",
               showConfirmButton: false,
-              timer: 1500
+              timer: 1500,
             });
-            localStorage.clear()
+            setCartList([]);
             setTimeout(() => {
-              window.location.href = '/';
             }, 2000);
-          } else if (result.isDenied) {
-            Swal.fire({
-              title: "Tu compra no ha sido procesada",
-              icon: 'error',
-              background: "rgba(255, 255, 255, 0.80)",
-              color: "black",
-              confirmButtonColor: "green"
-            })
-          }
-        })
+          })
+      } else if (result.isDenied) {
+        Swal.fire({
+          title: "Tu compra no ha sido procesada",
+          icon: "error",
+          background: "rgba(255, 255, 255, 0.80)",
+          color: "black",
+          confirmButtonColor: "green",
+        });
       }
-
+    });
+  };
 
   return (
-    <CartContext.Provider value={{
+    <CartContext.Provider
+      value={{
         cartList,
         addProduct,
         deleteCart,
@@ -100,13 +142,11 @@ export const CartContextProvider = ({children}) => {
         precioTotal,
         eliminarProducto,
         confirmarCompra,
-    }}>
-        {children}
+      }}
+    >
+      {children}
     </CartContext.Provider>
-    
-  )
-}
+  );
+};
 
-
-
-export default CartContextProvider
+export default CartContextProvider;
